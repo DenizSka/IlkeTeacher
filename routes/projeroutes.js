b8b3db05014621d01469c1952d353c619c29ec6f
Deniz Skantz <denizskantz@hotmail.com>
2018-03-26 16:33:03 -0400

    error fix

--- routes/projeroutes.js
+++ routes/projeroutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const projeRoutes = express.Router();
 const controller = require('../controllers/projeController')
-
+const views = require('../controllers/viewsController');
 
 
 // the root route,
--- server.js
+++ server.js
@@ -9,40 +9,42 @@ const logger = require('morgan');
 //telling which logger to use.
 const ejs = require('ejs');
 const path = require('path');
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
 
 // set the port, either from an environmental variable or manually
 const port = process.env.PORT || 3000;
 
 /* Views */
+app.set('views', path.join( __dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(express.static('public'));
 // parse incoming data
 /* we'll be reading the form body, but not accepting files,
 or anything more than text*/
-app.use( bodyParser.urlencoded({ extended: false }));
+app.use( bodyParser.urlencoded({ extended: true }));
 /* we'll also be accepting and parsing json  */
 app.use(bodyParser.json());
 
-// Setting up the app to sent back an actual index page.
-app.use(express.static('public'));
+// static route to public
+// This sets a folder called public to be the destination from which any static assets (images,css,etc) will be served.
+app.use( '/static', express.static( path.join( __dirname, 'public' )));
 
 
 // below the index route
 app.use('/projeler', projeRoutes);
 
 // home route
-app.get('/', (req,res) => res.render('pages/home', {data: pageTitle}));
-
-// static route to public
-// This sets a folder called public to be the destination from which any static assets (images,css,etc) will be served.
-app.use( express.static( path.join( __dirname, 'public' )));
+app.get('/', (req,res) => res.render('pages/home', {data: 'Hello'}));
 
 
 // get anything that hasn't already been matched
 app.use('*', (req, res) => {
     // send a response with status 404
-    res.status(404).send(err);
+    res.status(404).send('page not found');
 });
 
 
